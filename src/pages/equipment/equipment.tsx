603c857e98606c9468641fcd25b38693dd4f49b4
Oleh <oleh@surfer-172-29-7-153-hotspot.internet-for-guests.com>
2024-07-12 04:04:51 +0200

    -h

---
+++ src/pages/equipment/equipment.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { pagesData } from "../pagesData/pages";
+import { Box, Typography } from "@mui/material";
+import { CardWrapper } from "../helpers/cardWrapper";
+import { colors } from "../../media/colorsModule";
+
+export const Equipment:React.FC = () => {
+	return (
+		<section style={{backgroundColor: colors.color_3, maxWidth: '1200px', marginInline: 'auto', width: '90%'}}>
+			<Typography variant="h3" sx={{marginInline: 'auto', width: 'fit-content', position: 'relative', top: {xs: '100px', lg: '200px'}, marginBottom: {xs: '50px', lg: '80px'}, color: colors.color_grey}}>Оборудование</Typography>
+			<Box sx={{display: 'flex', flexWrap: 'wrap', justifyContent: 'space-around', maxWidth: 1200, width: '90%', marginInline: 'auto', position: 'relative', top: {xs: '80px', lg: '160px'}, paddingBottom: {xs: '100px', lg: '160px'}}}>
+				{pagesData.slice(9, 11).map((page, index) => (
+          			<CardWrapper key={index} {...page} />
+        		))}			
+			</Box>		
+		</section>
+	)
+}
